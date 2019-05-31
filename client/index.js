083e86d957df291849f07e08ffd365fff7b03835
Harriet Ashcroft <harriet.ashcroft@gmail.com>
2019-05-31 15:17:55 -0400

    tests adding event listeners

--- client/index.js
+++ client/index.js
@@ -4,5 +4,11 @@ const newDiv = document.createElement("div");
 const newContent = document.createTextNode("Hi there and greetings!");
 // add the text node to the newly created div
 newDiv.appendChild(newContent);
+document.getElementById('root').appendChild(newDiv);
+const buttons = document.getElementsByTagName('button');
 
-document.getElementById('root').appendChild(newDiv)
+for (let btn of buttons) {
+  btn.addEventListener('click', () => {
+    newDiv.innerHTML = 'TEST'
+  })
+}
