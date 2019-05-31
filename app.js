db0a84fe8f4f53a12c7750f43ebd5ad70a99e2b4
Harriet Ashcroft <harriet.ashcroft@gmail.com>
2019-05-31 13:29:55 -0400

    adds some routing

--- app.js
+++ app.js
@@ -4,9 +4,36 @@ const hostname = process.env.HOST || '127.0.0.1';
 const port = process.env.PORT || 3005;
 
 const server = http.createServer((req, res) => {
+  let {method, url} = req;
+
+  req.on('error', (err) => {
+    console.log(err)
+  })
+
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello World\n');
-}).listen(port, hostname, () => {
+
+  //rudimentary routing
+  switch(url) {
+    case '/whassup':
+      res.end('The Sky is Up\n');
+      break;
+    case '/hello':
+      res.end('Hello World\n');
+      break;
+    case '/humanError':
+      throw "A Fake Error";
+      break;
+    default:
+      res.end('Hello World\n');
+  }
+})
+
+server.listen(port, hostname, () => {
   console.log(`WOOT Server running at http://${hostname}:${port}/`)
+})
+
+server.on('error', (err) => {
+  console.log('WOW! what a failure!')
+  console.log(err)
 })
