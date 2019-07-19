10a9df3eaf0bebf4dcce2e1e6d3ae1094d38e797
Harriet Ashcroft <harriet.ashcroft@gmail.com>
2019-07-19 16:09:00 -0400

    Adds cleanup

--- client/src/components/Components.test.js
+++ client/src/components/Components.test.js
@@ -1,4 +1,8 @@
-import { fixture } from '@open-wc/testing-helpers';
+import { fixture, fixtureCleanup } from '@open-wc/testing-helpers';
+
+afterEach(() => {
+  fixtureCleanup();
+});
 
 it('can instantiate an element', async () => {
   const el = await fixture('<my-el foo="bar"></my-el>');
